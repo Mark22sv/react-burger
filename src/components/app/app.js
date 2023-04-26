48c017e0f09c4b8da2a5771504332d2e6aaee418
Mark22sv <116151585+Mark22sv@users.noreply.github.com>
2023-04-26 10:59:23 +0600

    appHeader

---
+++ src/components/app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AppHeader from '../appheader/appheader';
+
+class App extends React.Component {
+  render() {
+    return (
+
+      <div>
+        <AppHeader />
+       
+      </div>
+
+    );
+  }
+}
+
+export default App;
